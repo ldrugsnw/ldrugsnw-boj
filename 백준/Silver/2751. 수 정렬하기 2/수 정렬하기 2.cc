2707eed8084d374fd47437173ec78c1ce23a5223
신준혁 <164439042+ldrugsnw@users.noreply.github.com>
2025-07-17 09:07:03 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 284 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n = 10;
+int arr[1000001];
+int tmp[1000001];
+
+void merge(int st, int en){
+    int mid = (st + en) / 2;
+    
+    int left = st;
+    int right = mid;
+    
+    for(int i = st ; i < en ; i++){
+        if(right == en){
+            tmp[i] = arr[left];
+            left++;
+        }
+        
+        else if(left == mid){
+            tmp[i] = arr[right];
+            right++;
+        }
+        
+        else if(arr[left] <= arr[right]){
+            tmp[i] = arr[left];
+            left++;
+        }
+        
+        else{
+            tmp[i] = arr[right];
+            right++;
+        }
+    }
+    
+    for(int i = st ; i < en ; i++){
+        arr[i] = tmp[i];
+    }
+}
+
+void merge_sort(int st, int en){
+    if(en - st == 1){
+        return;
+    }
+    
+    int mid = (st+en)/2;
+    
+    merge_sort(st, mid);
+    merge_sort(mid, en);
+    merge(st, en);
+}
+
+
+
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+   
+    merge_sort(0, n);
+    for(int i = 0 ; i < n ; i++){
+        cout << arr[i] << '\n';
+    }
+    
+    return 0;
+}
