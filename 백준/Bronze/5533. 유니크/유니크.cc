dda7e29e9c081a67d4f5aa1962c4d22d1e8e6be8
신준혁 <164439042+ldrugsnw@users.noreply.github.com>
2025-08-12 20:59:07 +0900

    [Bronze I] Title: 유니크, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5533. 유니크/유니크.cc
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 5533                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: ldrugsnw <boj.kr/u/ldrugsnw>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/5533                           #+#        #+#      #+#    */
+/*   Solved: 2025/08/12 20:41:31 by ldrugsnw      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int games[201][4]; // [사람번호, 게임 번호] =
+int scores[201];
+
+int main(){
+    int N;
+    cin >> N;
+
+    for(int num = 1 ; num <= N; num++){
+        for(int game = 1 ; game <= 3 ; game++){
+            cin >> games[num][game];
+        }
+    }
+
+    // 게임 시작!!!!!!
+
+    for(int game = 1 ; game <= 3 ; game++){
+        for(int i = 1 ; i <= N ; i++){ // 사람
+            bool PS = true;
+            for(int k = 1 ; k <= N ; k++){
+                if(games[i][game] == games[k][game] && i != k){
+                    PS = false;
+                }
+            }
+            if(PS){
+                scores[i] += games[i][game];
+            }
+        }
+    }
+
+    for(int i = 1 ; i<= N ; i++){
+        cout << scores[i] << '\n';
+    }
+}
