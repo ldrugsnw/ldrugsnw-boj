bf1c62906cb94fac2c70d71b07403635e15304f3
신준혁 <164439042+ldrugsnw@users.noreply.github.com>
2025-10-02 23:34:26 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 40 ms, Memory: 7336 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pp pair<int, int>
+#define ttt tuple<int, int, int>
+#define tttt tuple<int, int, int, int>
+
+int N, M;
+char board[1001][1001];
+bool visited[1001][1001][2];
+
+// 상 하 좌 우
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+void input(){
+    cin >> N >> M;
+
+
+    for(int i = 0 ; i < N ; i++){
+        string s;
+        cin >> s;
+        for(int j = 0 ; j < M ; j++){
+            board[i][j] = s[j];
+        }
+    }
+}
+
+/// i <- y , j <- x
+
+int bfs() {
+    queue<tuple<int, int, int, int>> q;
+
+     if(N == 1 && M == 1){
+        return 1;
+    }
+
+    q.push({0, 0, 0, 1}); 
+    visited[0][0][0] = true;
+
+    while (!q.empty()) {
+        auto [y, x, broken, dist] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+
+            if (ny == N - 1 && nx == M - 1) {
+                return dist + 1;
+            }
+
+            if (board[ny][nx] == '0' && !visited[ny][nx][broken]) {
+                visited[ny][nx][broken] = true;
+                q.push({ny, nx, broken, dist + 1});
+            }
+
+            else if (board[ny][nx] == '1' && broken == 0 && !visited[ny][nx][1]) {
+                visited[ny][nx][1] = true; 
+                q.push({ny, nx, 1, dist + 1});
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+    fastio;
+    input();
+
+    cout << bfs() << '\n';
+    
+    return 0;
+}
